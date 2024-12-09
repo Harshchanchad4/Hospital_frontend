ea1820079a5c6264d61355b2747911aea91fef1a
Harshchanchad4 <150885764+Harshchanchad4@users.noreply.github.com>
2024-12-09 11:21:51 +0530

    Initial commit

--- server/app.js
+++ server/app.js
@@ -16,17 +16,15 @@ const { errorMiddleware } = require("./middlewares/errorMiddleware");
 
 cloudinaryConnect();
 
-
 const corsOptions = {
-    origin: [process.env.FRONTEND_URL1 ,process.env.FRONTEND_URL2 , process.env.FRONTEND_URL3 ,process.env.FRONTEND_URL4 ],  // Update this to your frontend's URL
-    // Update this to your frontend's URL
+    origin: [process.env.FRONTEND_URL1 , process.env.FRONTEND_URL2 , process.env.FRONTEND_URL3], // Update this to your frontend's URL
     credentials: true,
     optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
-app.use(cookieParser());
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -51,13 +49,6 @@ app.get("/", () => {
 
 database.connect();
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, '0.0.0.0', () => {
-	console.log(`App is running at ${PORT}`);
-}).on('error', (err) => {
-	console.error('Failed to start server:', err);
+app.listen(4000, () => {
+    console.log("Server started listening on port 4000");
 });
-
-
-
